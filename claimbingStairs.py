318f77c587eafc4c50119e1d3a711fc588733ac8
Santhosh Kumar MS <152013656+santhosh-kumar-ms@users.noreply.github.com>
2024-02-07 16:56:08 +0530

    Update claimbingStairs.py

--- claimbingStairs.py
+++ claimbingStairs.py
@@ -1,3 +1,4 @@
+# Space Optimization
 def climbStairs(self, n: int) -> int:
         one, two = 1, 1
         for i in range(n - 1):
@@ -5,3 +6,42 @@ def climbStairs(self, n: int) -> int:
             one = one + two
             two = temp
         return one
+
+#############################################################################################################################################################################################
+
+#Recursion
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 0 or n == 1:
+            return 1
+        return self.climbStairs(n-1) + self.climbStairs(n-2)
+
+#############################################################################################################################################################################################
+
+#Memoization
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+        return self.helper(n, memo)
+    
+    def helper(self, n: int, memo: dict[int, int]) -> int:
+        if n == 0 or n == 1:
+            return 1
+        if n not in memo:
+            memo[n] = self.helper(n-1, memo) + self.helper(n-2, memo)
+        return memo[n]
+            
+############################################################################################################################################################################################
+
+#Tabulation
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 0 or n == 1:
+            return 1
+
+        dp = [0] * (n+1)
+        dp[0] = dp[1] = 1
+        
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
