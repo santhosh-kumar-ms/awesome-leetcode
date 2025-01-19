6e05ab21585be9d4ed4355ea10babda6843b7e91
santhosh-kumar-ms <abstractwithboundaries@gmail.com>
2025-01-19 14:13:26 +0530

    added rotateImage.py

---
+++ rotateImage.py
@@ -0,0 +1,9 @@
+def rotate(self, matrix: List[List[int]]) -> None:
+        size = len(matrix)
+
+        for i in range(size):
+            for j in range(i + 1, size):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        for i in range(size):
+            matrix[i].reverse()
